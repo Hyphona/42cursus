8a39dae9c10baded4a36d530611de5bab6f58ab1
Alexis Perrot <alperrot@2B1.42angouleme.fr>
2023-11-07 11:48:38 +0100

    Fixed ft_strlen

--- 42cursus/Libft/ft_strlen.c
+++ 42cursus/Libft/ft_strlen.c
@@ -6,13 +6,14 @@
 /*   By: alperrot <alperrot@student.42angouleme.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 16:14:06 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/06 16:15:35 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/07 11:45:49 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-int	ft_strlen(char *str)
+size_t	ft_strlen(const char *str)
 {
-	int	i;
+	long unsigned int	i;
 
 	i = 0;
 	while (str[i])
--- 42cursus/Libft/libft.h
+++ 42cursus/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 10:40:19 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/07 10:55:33 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/07 11:47:54 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -26,8 +26,9 @@ void	*ft_memset(void *str, int c, size_t size);
 char	*ft_strchr(const char *str, int c);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-int	ft_strlen(char *str);
+size_t	ft_strlen(const char *str);
 int	ft_strncmp(const char *str1, const char *str2, size_t size);
+char	*ft_strnstr(const char *str1, const char *str2, size_t max);
 char	*ft_strrchr(const char *str, int c);
 int	ft_tolower(int arg);
 int	ft_toupper(int arg);
