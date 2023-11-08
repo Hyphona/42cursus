205d2c2ce016baf8a9539bd2367be1f97306d465
Hyphona <hyphona@gmail.com>
2023-11-08 20:03:22 +0100

    A bit of optimization

--- 42cursus/Libft/ft_memchr.c
+++ 42cursus/Libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:46:43 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/08 18:02:59 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/08 20:01:24 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,7 +16,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned char	*ptr;
 
 	ptr = (unsigned char *) s;
-	while (n-- > 0)
+	while (n--)
 	{
 		if (*ptr == (unsigned char) c)
 			return ((void *) ptr);
