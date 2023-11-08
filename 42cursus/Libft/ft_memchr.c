a6e0169a9498631e6ffb2b9a1f3a98e7a646175f
Hyphona <hyphona@gmail.com>
2023-11-08 18:04:14 +0100

    Fixed ft_memchr

--- 42cursus/Libft/ft_memchr.c
+++ 42cursus/Libft/ft_memchr.c
@@ -6,26 +6,21 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:46:43 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/07 20:14:11 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/08 18:02:59 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
 	unsigned char	*ptr;
 
-	i = 0;
 	ptr = (unsigned char *) s;
-	if (n > 0)
+	while (n-- > 0)
 	{
-		while (i <= n && ptr[i] != '\0')
-		{
-			if (ptr[i] == c)
-				return (&ptr[i]);
-			i++;
-		}
+		if (*ptr == (unsigned char) c)
+			return ((void *) ptr);
+		ptr++;
 	}
 	return ((void *) 0);
 }
