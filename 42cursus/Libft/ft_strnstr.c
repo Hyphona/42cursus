9f0e3627239115807e3840b8ffd5800b8d8d5549
Hyphona <hyphona@gmail.com>
2023-11-11 14:33:18 +0100

    Fixed ft_strnstr

--- 42cursus/Libft/ft_strnstr.c
+++ 42cursus/Libft/ft_strnstr.c
@@ -6,32 +6,30 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:05:25 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/07 20:38:55 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/11 14:32:37 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strnstr(const char *str1, const char *str2, size_t size)
 {
-	size_t	i;
-	size_t	j;
+	int	i;
+	int	j;
 
 	i = 0;
-	j = 0;
-	if (size > i)
+	if (!str2[i] || !size)
+		return ((char *) str1);
+	while (str1[i] && (i < (int) size))
 	{
-		if (str2[i] == '\0')
-			return ((char *) str1);
-		while (str2[j])
+		j = 0;
+		while (str1[i + j] == str2[j] && (i + j) < (int) size)
 		{
-			while (str1[i])
-			{
-				if (str1[i] == str2[j])
-					return ((char *) &str1[i]);
-				i++;
-			}
+			if (!str2[j + 1])
+				return ((char *) &str1[i]);
 			j++;
 		}
+		i++;
 	}
 	return ((void *) 0);
 }
