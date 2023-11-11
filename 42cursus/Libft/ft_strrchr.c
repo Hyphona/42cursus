331c24886530c8c6c40d5666e015732341c12ef4
Hyphona <hyphona@gmail.com>
2023-11-11 11:09:06 +0100

    Fixed ft_strrchr

--- 42cursus/Libft/ft_strrchr.c
+++ 42cursus/Libft/ft_strrchr.c
@@ -3,29 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alperrot <alperrot@student.42angouleme.fr  +#+  +:+       +#+        */
+/*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:48:28 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/08 10:49:34 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/11 11:08:47 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strrchr(const char *str, int c)
-{
-	int	i;
+#include "libft.h"
 
-	i = 0;
-	if (str[i] != '\0')
-	{
-		while (str[i])
-		{
-			if (str[i] == c && str[i + 1] != '\0')
-			{
-				i++;
-				return (ft_strrchr(&str[i], c));
-			}
-			i++;
-		}
-	}
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t			len;
+
+	len = ft_strlen(s);
+	if (s[len] == '\0' && (char) c == '\0')
+		return ((char *) &s[len]);
+	while (len-- > 0)
+		if (s[len] == (char) c)
+			return ((char *) &s[len]);
 	return ((void *) 0);
 }
