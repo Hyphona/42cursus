ae0b132691a2e027944f1d951033fb4234e413e6
Hyphona <hyphona@gmail.com>
2023-11-15 19:52:25 +0100

    ft_split (again) (not normed)

--- 42cursus/Libft/ft_split.c
+++ 42cursus/Libft/ft_split.c
@@ -6,105 +6,68 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 09:30:10 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/15 13:30:26 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/15 19:42:30 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-static int	ft_countword(char const *s, char c)
+static size_t	ft_countword(char const *s, char c)
 {
-	int	i;
-	int	w;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
-	w = 0;
+	count = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
-		{
-			while (s[i] == c)
-				i++;
-			if (s[i])
-				w++;
-		}
+		if ((i == 0 && s[i] != c) || (s[i] != c && s[i - 1] == c))
+			count++;
 		i++;
 	}
-	printf("(ft_countword) return value: %d\n", w); //DEBUG
-	return (w);
+	return (count);
 }
 
-static char	**ft_free(char **tab)
+static char	**ft_freeall(char **tab, size_t j)
 {
-	int	i;
-
-	i = 0;
-	while (tab[i])
+	while (j > 0)
 	{
-		printf("(ft_free) Attempt to free tab[%d]", i); //DEBUG
-		free(tab[i]);
-		printf(" | tab[%d] got free!\n", i); //DEBUG
-		i++;
+		free(tab[j]);
+		j--;
 	}
 	free(tab);
-	printf("(ft_free) **tab got completely free!\n"); //DEBUG
 	return ((char **) 0);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	int	i;
-	int	j;
-	int	wcount;
-	char	**astr;
+	size_t	i;
+	size_t	j;
+	size_t	k;
+	char	**tab;
 
-	if (!s || !c)
+	if ((!s || !c))
 		return ((char **) 0);
 	i = 0;
 	j = 0;
-	wcount = 0;
-	astr = malloc(sizeof(char *) * ft_countword(s, c) + 1);
-	if (!astr)
+	tab = ft_calloc(sizeof(char *), (ft_countword(s, c) + 1));
+	if (!tab)
 		return ((char **) 0);
 	while (s[i])
 	{
-		if (s[j] == c && s[j + 1] == c && s[j])
+		if (s[i] != c)
 		{
-			while (s[j] == c)
-			{
+			k = i;
+			while (s[i] && s[i] != c)
 				i++;
-				j++;
-			}
-		}
-		while (s[j] != c && s[j])
+			tab[j] = ft_substr(s, k, i - k);
+			if (!tab[j])
+				return (ft_freeall(tab, j));
 			j++;
-		if (s[j - 1] != c)
-		{
-			astr[wcount] = ft_substr(s, i, j - i);
-			if (!astr[wcount])
-				return (ft_free(astr));
-			wcount++;
 		}
-		j++;
-		i = j;
+		else
+			i++;
 	}
-	return (astr);
-}
-
-// TESTS //
-int	main()
-{
-	int	i;
-	char	**tab;
-
-	i = 0;
-	tab = ft_split("alexis.....l..e.....x.......i....s........", '.');
-	while (tab[i])
-	{
-		printf("(ft_split) Value of tab[%d] : %s\n", i, tab[i]);
-		i++;
-	}
-	ft_free(tab);
+	tab[j] = (char *) 0;
+	return (tab);
 }
