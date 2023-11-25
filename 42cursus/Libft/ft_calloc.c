34e83aeb1dca706f4871d13f37fb342543b02619
Hyphona <hyphona@gmail.com>
2023-11-25 12:44:49 +0100

    norminette

--- 42cursus/Libft/ft_calloc.c
+++ 42cursus/Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 15:33:35 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/25 12:41:39 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/25 12:44:36 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*str;
-	
+
 	if (!nmemb || !size)
 		return (ft_calloc(1, 1));
 	if (nmemb > 2147483647 / size)
