7d6249587797f75e34296091f63b33a43294f1ac
Hyphona <hyphona@gmail.com>
2023-11-25 13:02:13 +0100

    Fixed ft_strtrim

--- 42cursus/Libft/ft_strtrim.c
+++ 42cursus/Libft/ft_strtrim.c
@@ -6,46 +6,36 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 14:12:34 by alperrot          #+#    #+#             */
-/*   Updated: 2023/11/15 19:49:57 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/11/25 12:57:19 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_isspace(char const *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] != ' ' && !(str[i] >= '\t' && str[i] <= '\r'))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	j;
-	size_t	len;
-	char	*nstr;
+	int		i;
+	int		j;
+	int		k;
+	char	*str;
 
-	if ((!s1 || !set) || (ft_isspace(s1) && ft_isspace(set)))
-		return (ft_strdup(""));
+	if (!s1 || !set)
+		return ((void *) 0);
 	i = 0;
+	j = ft_strlen(s1) - 1;
 	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	len = ft_strlen(s1);
-	while (len > 0 && ft_strchr(set, s1[len - 1]))
-		len--;
-	nstr = ft_calloc(sizeof(char), len - i + 1);
-	if (!nstr)
+	while (j > i && ft_strchr(set, s1[j]))
+		j--;
+	str = ft_calloc(sizeof(char), j - i + 2);
+	if (!str)
 		return ((void *) 0);
-	j = 0;
-	while (i < len)
-		nstr[j++] = s1[i++];
-	return (nstr);
+	k = 0;
+	while (i <= j)
+	{
+		str[k] = s1[i];
+		i++;
+		k++;
+	}
+	return (str);
 }
