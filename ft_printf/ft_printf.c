7a164c484be5861f360532c2b4f102a2e2d79f68
Hyphona <hyphona@gmail.com>
2023-12-13 15:41:10 +0100

    Added c flag support as a test

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,22 +6,44 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 13:33:18 by alperrot          #+#    #+#             */
-/*   Updated: 2023/12/12 15:55:21 by alperrot         ###   ########.fr       */
+/*   Updated: 2023/12/13 15:40:08 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
+#include <stdarg.h>
 
-int	ft_printf(const char *format, ...)
+static int	ft_printf_parser(const char c, void *value)
 {
 	size_t	lenght;
 
+	lenght = 0;
+	if (c == 'c')
+	{
+		write(1, (char *) &value, 1);
+		return (1);
+	}
+	return (lenght);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	size_t	lenght;
+
+	va_start(args, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			//
+			format++;
+			lenght += ft_printf_parser(*format, va_arg(args, void *));
+			format ++;
 		}
+		write(1, format, 1);
+		lenght++;
+		format++;
 	}
+	va_end(args);
 	return (lenght);
 }
