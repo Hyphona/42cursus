56d3f094f72903b4af71e8ab33496173a60833d7
Alexis <hyphona@gmail.com>
2024-01-02 09:37:41 +0000

    Added ft_printf tests

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 13:33:18 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/02 09:00:58 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/02 09:36:10 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ static int	ft_printf_formatter(const char type, void *value)
 	if (type == 'p')
 	{
 		write(1, "0x", 2);
-		return (ft_putnbr_base_fd(*((int *) &value), lower_base, 1) + 2);
+		return (ft_putnbr_base_fd(*((unsigned long int *) &value), lower_base, 1) + 2);
 	}
 	if (type == 'i' || type == 'd')
 		return (ft_putnbr_fd(*((int *) &value), 1));
@@ -81,3 +81,29 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (lenght);
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char			*p;
+	unsigned int	i;
+	
+	i = 4294967295;
+	printf("   printf : %%c : %c\n", 'a');
+	ft_printf("ft_printf : %%c : %c\n", 'a');
+	printf("   printf : %%s : %s\n", "0123456789abcdef");
+	ft_printf("ft_printf : %%s : %s\n", "0123456789abcdef");
+	printf("   printf : %%p : %p\n", &p);
+	ft_printf("ft_printf : %%p : %p\n", &p);
+	printf("   printf : %%i : %i\n", -42);
+	ft_printf("ft_printf : %%i : %i\n", -42);
+	printf("   printf : %%d : %d\n", 42);
+	ft_printf("ft_printf : %%d : %d\n", 42);
+	printf("   printf : %%u : %u\n", i);
+	ft_printf("ft_printf : %%u : %u\n", i);
+	printf("   printf : %%x : %x\n", 0x45de6fa);
+	ft_printf("ft_printf : %%x : %x\n", 0x45de6fa);
+	printf("   printf : %%X : %X\n", 0x45de6fa);
+	ft_printf("ft_printf : %%X : %X\n", 0x45de6fa);
+}
+*/
