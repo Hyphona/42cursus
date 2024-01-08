76c21f7a7498cef000d74a62afad795245f3d644
Alexis <hyphona@gmail.com>
2024-01-08 12:20:08 +0000

    First idea

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,25 +6,61 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:03:55 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/08 10:01:18 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/08 12:17:21 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+static size_t   ft_linelen(char *file)
 {
-    //static int  line;
-    
-    if (!fd || BUFFER_SIZE <= 0)
-        return ((void *) 0);
-    return ((void *) 0);
+    size_t  len;
+
+    len = 0;
+    while (*file != '\n')
+    {
+        file++;
+        len++;
+    }
+    return (len + 1);
 }
 
-/*
+char    *get_next_line(int fd)
+{
+    static int  last;
+    size_t      len;
+    int         file;
+    char        *line;
+    char        *buffer;
+    
+    if (!fd || BUFFER_SIZE <= 1)
+        return ((void *) 0);
+    buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+    if (!buffer)
+        return ((void *) 0);
+    file = read(fd, buffer, BUFFER_SIZE);
+    if (!file)
+        return ((void *) 0);
+    len = ft_linelen(&buffer[last]);
+    line = malloc(sizeof(char) * len + 1);
+    if (!line)
+        return ((void *) 0);
+    line[len] = '\n';
+    len--;
+    while (len != 0)
+    {
+        line[len] = buffer[len + last];
+        len--;
+    }
+    last += ft_linelen(&buffer[last]);
+    free(buffer);
+    return (line);
+}
+
 #include <stdio.h>
+#include <fcntl.h>
 int main(void)
 {
     printf("BUFFER_SIZE = %i\n", BUFFER_SIZE);
+    printf("%s", get_next_line(open("./text.txt", O_RDONLY)));
 }
-*/
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:03:52 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/08 10:00:49 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/08 10:28:23 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
+# endif
 
 #include <unistd.h>
 #include <stdlib.h>
 
 char    *get_next_line(int fd);
 
-# endif
 #endif
