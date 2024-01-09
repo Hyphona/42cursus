b52bb69e10428e8cf41d6acef01bcaaa4de06ace
Alexis <hyphona@gmail.com>
2024-01-09 12:47:44 +0000

    I have introduced a segfault (no need to thanks me)

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:03:55 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/08 12:17:21 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/09 12:43:41 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,50 +17,59 @@ static size_t   ft_linelen(char *file)
     size_t  len;
 
     len = 0;
-    while (*file != '\n')
+    while (*file)
     {
+        if (*file == '\n')
+            return (len + 1);
         file++;
         len++;
     }
-    return (len + 1);
+    return (len);
 }
 
 char    *get_next_line(int fd)
 {
     static int  last;
-    size_t      len;
-    int         file;
-    char        *line;
+    size_t      l_len;
     char        *buffer;
-    
-    if (!fd || BUFFER_SIZE <= 1)
+    char        *line;
+    int         file;
+
+    if (!fd || !BUFFER_SIZE)
         return ((void *) 0);
-    buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+    buffer = malloc((sizeof(char) * BUFFER_SIZE) + 1);
     if (!buffer)
         return ((void *) 0);
     file = read(fd, buffer, BUFFER_SIZE);
     if (!file)
         return ((void *) 0);
-    len = ft_linelen(&buffer[last]);
-    line = malloc(sizeof(char) * len + 1);
+    l_len = ft_linelen(buffer);
+    line = malloc((sizeof(char) * l_len) + 1);
     if (!line)
         return ((void *) 0);
-    line[len] = '\n';
-    len--;
-    while (len != 0)
+    while (*buffer != '\n')
     {
-        line[len] = buffer[len + last];
-        len--;
+        if (*buffer == '\n')
+        {
+            *line = '\n';
+            last++;
+            free(buffer);
+            return (line);
+        }
+        *line = *buffer;
+        buffer++;
+        line++;
+        last++;
     }
-    last += ft_linelen(&buffer[last]);
     free(buffer);
     return (line);
 }
-
+/*
 #include <stdio.h>
 #include <fcntl.h>
 int main(void)
 {
     printf("BUFFER_SIZE = %i\n", BUFFER_SIZE);
-    printf("%s", get_next_line(open("./text.txt", O_RDONLY)));
+    printf("%s", get_next_line(open("text.txt", O_RDONLY)));
 }
+*/
