374faa9013cdf702951b84555b8f430551be3fe2
Alexis <hyphona@gmail.com>
2024-01-17 10:36:09 +0000

    Fix in progress

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,67 +6,35 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:03:55 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/16 10:25:16 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/17 10:31:01 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static size_t   ft_strlen(char const *str)
-{
-    size_t  i;
-
-    i = 0;
-    while(*str)
-    {
-        str++;
-        i++;
-    }
-    return(i);
-}
-
-char	*ft_strjoin(char const *s1, char const*s2)
-{
-	char	*str;
-	size_t	i;
-	size_t	j;
-
-	if (!s1 || !s2)
-		return ((void *) 0);
-	str = malloc((sizeof(char) * (ft_strlen(s1) + ft_strlen(s2))) + 1);
-	if (!str)
-		return ((void *) 0);
-	i = 0;
-	j = 0;
-	while (s1[i])
-		str[j++] = s1[i++];
-	i = 0;
-	while (s2[i])
-		str[j++] = s2[i++];
-	return (str);
-}
-
-static int  ft_hasline(char *line)
-{
-    while (*line)
-    {
-        if (*line == '\n')
-            return (1);
-        line++;
-    }
-    return (0);
-}
-
-static size_t   ft_linelen(char *file)
+static int  ft_hasline(char *buf)
 {
     size_t  len;
 
     len = 0;
-    while (*file)
+    while (buf[len])
     {
-        if (*file == '\n')
+        if (buf[len] == '\n')
+            return (len);
+        len++;
+    }
+    return (0);
+}
+
+static size_t   ft_linelen(char *str)
+{
+    size_t  len;
+
+    len = 0;
+    while (str[len])
+    {
+        if (str[len] == '\n')
             return (len + 1);
-        file++;
         len++;
     }
     return (len);
@@ -98,41 +66,49 @@ static char    *ft_linecpy(char *tmp)
 
 char    *get_next_line(int fd)
 {
-    static size_t   offset;
+    //static size_t   offset;
     char            *buf;
     char            *tmp;
     int             state;
 
     if (!fd || !BUFFER_SIZE)
-        return ((void *) 0);
+        return((void *) 0);
     buf = malloc((sizeof(char) * BUFFER_SIZE) + 1);
     if (!buf)
-        return((void *) 0);
+        return ((void *) 0);
     state = -1;
     tmp = "";
-    if (offset > 0)
-            state = read(fd, buf, offset);
-    while (state != 0)
+    /*if (offset > 0)
+        state = read(fd, buf, offset);*/
+    while (state)
     {
         state = read(fd, buf, BUFFER_SIZE);
+        buf[state] = 0;
         tmp = ft_strjoin(tmp, buf);
-        offset = offset + ft_linelen(tmp);
+        //offset = offset + ft_linelen(buf);
         if (ft_hasline(buf))
             break ;
     }
     free(buf);
     return (ft_linecpy(tmp));
 }
-/*
+
 #include <stdio.h>
 #include <fcntl.h>
 int main(void)
 {
-    printf("BUFFER_SIZE = %i\n", BUFFER_SIZE);
-    printf("%s", get_next_line(open("text.txt", O_RDONLY)));
-    printf("%s", get_next_line(open("text.txt", O_RDONLY)));
-    printf("%s", get_next_line(open("text.txt", O_RDONLY)));
-    printf("%s", get_next_line(open("text.txt", O_RDONLY)));
-    printf("%s", get_next_line(open("text.txt", O_RDONLY)));
+    char    *str;
+    int     fd;
+    int i;
+
+    i = 0;
+    fd = open("text.txt", O_RDONLY);
+    while (i < 5)
+    {
+        str = get_next_line(fd);
+        printf("%s", str);
+        free(str);
+        i++;
+    }
+    close(fd);
 }
-*/
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 08:38:41 by alperrot          #+#    #+#             */
+/*   Updated: 2024/01/17 09:15:56 by alperrot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void    ft_bzero(char *str)
+{
+    size_t  len;
+
+    len = 0;
+    while (str[len])
+    {
+        str[len] = '\0';
+        len++;
+    }
+}
+
+size_t  ft_strlen(const char *str)
+{
+    size_t  len;
+
+    len = 0;
+    while (str[len])
+        len++;
+    return(len);
+}
+
+char	*ft_strjoin(char const *s1, char const*s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return ((void *) 0);
+	str = malloc((sizeof(char) * (ft_strlen(s1) + ft_strlen(s2))) + 1);
+	if (!str)
+		return ((void *) 0);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+    str[j] = '\0';
+	return (str);
+}
