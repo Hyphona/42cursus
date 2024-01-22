aee5d34d54c5962af66e52f9c2902d2c21fee9c7
Alexis <hyphona@gmail.com>
2024-01-22 08:48:03 +0000

    Update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:03:55 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/17 12:04:05 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/22 08:47:52 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,29 +64,9 @@ static char    *ft_linecpy(char *tmp)
     return (dst);
 }
 
-static void ft_savelast(char *buf, char *dst)
-{
-    size_t  i;
-    size_t  j;
-
-    i = 0;
-    j = 0;
-    while (buf[i] != '\n')
-        i++;
-    i++;
-    while (buf[i])
-    {
-        dst[j] = buf[i];
-        i++;
-        j++;
-    }
-    dst[j] = '\0';
-}
-
 char    *get_next_line(int fd)
 {
-    static char     *last;
-    char            *buf;
+    static char     *buf;
     char            *tmp;
     int             state;
 
@@ -101,17 +81,12 @@ char    *get_next_line(int fd)
     {
         state = read(fd, buf, BUFFER_SIZE);
         buf[state] = '\0';
-        if (last)
-            tmp = ft_strjoin(tmp, last);
         tmp = ft_strjoin(tmp, buf);
         if (ft_hasline(buf))
             break ;
     }
-    last = malloc((sizeof(char) * (BUFFER_SIZE - ft_hasline(buf))) + 1);
-    if (!last)
-        return ((void *) 0);
-    ft_savelast(buf, last);
-    free(buf);
+    if (buf == 0)
+        free(buf);
     return (ft_linecpy(tmp));
 }
 
