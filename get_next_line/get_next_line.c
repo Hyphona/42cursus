f05fc56705a12441e3f36acef165b07aff9089c6
Alexis <hyphona@gmail.com>
2024-01-30 08:48:07 +0000

    Code refactoring

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 09:03:55 by alperrot          #+#    #+#             */
-/*   Updated: 2024/01/29 15:23:06 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/01/30 08:47:21 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,15 +67,7 @@ static char *ft_sendline(char **str)
     
     i = 0;
     if (str[0][0] == 0)
-    {
-        dst = malloc(sizeof(char) * 1);
-        if (!dst)
-            return (0);
-        dst[0] = 0;
-        free(*str);
-        *str = NULL;
-        return (dst);
-    }
+        return (*str);
     else
         dst = malloc((sizeof(char) * ft_linelen(*str)) + 2);
     if (!dst)
@@ -106,26 +98,21 @@ char    *get_next_line(int fd)
         return (0);
     if (tmp && ft_hasline(tmp))
         return (ft_sendline(&tmp));
-    if (!tmp)
-        tmp = ft_strjoin(NULL, NULL);
     buf = malloc((sizeof(char) * BUFFER_SIZE) + 1);
     if (!buf)
         return (0);
-    buf[BUFFER_SIZE] = '\0';
     state = -1;
-    tmp = ft_strjoin(tmp, NULL);
     while (state)
     {
         state = read(fd, buf, BUFFER_SIZE);
         if (state == -1)
             return (0);
-        buf[state] = 0;
+        buf[state] = '\0';
         tmp = ft_strjoin(tmp, buf);
         if (ft_hasline(buf))
             break ;
     }
     free(buf);
-    buf = NULL;
     return (ft_sendline(&tmp));
 }
 
