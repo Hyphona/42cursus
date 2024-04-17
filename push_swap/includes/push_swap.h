fc0838edb8310bbea30914b63f1d291ef13f31c9
Hyphona <hyphona@gmail.com>
2024-04-17 16:19:27 +0200

    Fix norm error

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,15 +6,15 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 09:04:02 by alperrot          #+#    #+#             */
-/*   Updated: 2024/04/17 16:17:11 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/04/17 16:19:12 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 typedef struct s_stack
 {
