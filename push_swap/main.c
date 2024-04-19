bfe8763c137d2a39eecc819bf8f15effda4e4809
Hyphona <hyphona@gmail.com>
2024-04-19 14:50:23 +0200

    Fix norm errors

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,51 +6,72 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:27:56 by alperrot          #+#    #+#             */
-/*   Updated: 2024/04/19 14:15:16 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/04/19 14:50:00 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/push_swap.h"
 
-int	main(int ac, char **av)
+static int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static void	sub_parsing(char *av, t_stack *s_a)
 {
 	int		i;
 	char	*nb;
+
+	i = 0;
+	nb = "";
+	while (av[i])
+	{
+		while (ft_isspace(av[i]) && av[i])
+			i++;
+		while (!ft_isspace(av[i]) && av[i])
+		{
+			nb = ft_strjoin(nb, av[i]);
+			i++;
+		}
+		if (nb[0] != '\0')
+		{
+			add_node(&s_a, create_node(ft_atoi(nb)));
+			free(nb);
+			nb = "";
+			if (av[i])
+				i++;
+		}
+	}
+}
+
+static void	parse(char **av)
+{
 	t_stack	*s_a;
-	
+
+	s_a = NULL;
+	while (*av)
+	{
+		if (ft_hasspace(*av))
+			sub_parsing(*av, s_a);
+		else
+			add_node(&s_a, create_node(ft_atoi(*av)));
+		av++;
+	}
+}
+
+int	main(int ac, char **av)
+{
 	if (ac < 2 || !check_args(av))
 	{
 		write(1, "Error\n", 6);
 		return (0);
 	}
 	av++;
-	s_a = NULL;
-	nb = "";
-	while (*av)
-	{
-		if (ft_hasspace(*av))
-		{
-			i = 0;
-			while ((*av)[i])
-			{
-				while (ft_isspace((*av)[i]) && (*av)[i])
-					i++;
-				while (!ft_isspace((*av)[i]) && (*av)[i])
-				{
-					nb = ft_strjoin(nb, (*av)[i]);
-					i++;
-				}
-				add_node(&s_a, create_node(ft_atoi(nb)));
-				if (nb)
-					free(nb);
-				nb = "";
-				if ((*av)[i])
-					i++;
-			}
-		}
-		else
-			add_node(&s_a, create_node(ft_atoi(*av)));
-		av++;
-	}
+	parse(av);
 	return (0);
 }
