7a86bb0a8fb5dbcca7b018145f0c837e757dc72c
Hyphona <hyphona@gmail.com>
2024-04-24 21:43:47 +0200

    Add stack B + function to print the stacks values (debug only)

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,12 +6,41 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:27:56 by alperrot          #+#    #+#             */
-/*   Updated: 2024/04/24 19:38:15 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/04/24 21:42:46 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/push_swap.h"
-#include <stdio.h>
+#include <stdio.h> //DEBUG ONLY
+
+/*
+ * This function prints the stacks A and B (just for debug purposes)
+ */
+static void print_stack(t_stack *s_a, t_stack *s_b)
+{
+	while (s_a)
+	{
+		if (s_b){
+			printf("%d  %d\n", s_a->nb, s_b->nb);
+			s_a = s_a->next;
+			s_b = s_b->next;
+		}
+		else
+		{
+			printf("%d  -\n", s_a->nb);
+			s_a = s_a->next;
+		}
+	}
+	if (s_b)
+	{
+		while (s_b)
+		{
+			printf("-  %d\n", s_b->nb);
+			s_b = s_b->next;
+		}
+	}
+	printf("_  _\nA  B\n------------\n");
+}
 
 static t_stack	*init_stack_a(char **av)
 {
@@ -27,9 +56,21 @@ static t_stack	*init_stack_a(char **av)
 	return (s_a);
 }
 
+static t_stack	*init_stack_b(t_stack **s_a)
+{
+	t_stack	*s_b;
+
+	s_b = NULL;
+	push_b(s_a, &s_b);
+	print_stack(*s_a, s_b);
+	push_b(s_a, &s_b);
+	return (s_b);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack	*s_a;
+	t_stack	*s_b;
 
 	if (ac < 2)
 		return (0);
@@ -41,6 +82,13 @@ int	main(int ac, char **av)
 	av++;
 	s_a = init_stack_a(av);
 	if (is_sorted(s_a))
+	{
 		free_stack(s_a);
+		return (0);
+	}
+	s_b = NULL;
+	s_b = init_stack_b(&s_a);
+	free_stack(s_a);
+	free_stack(s_b);
 	return (0);
 }
