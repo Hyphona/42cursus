d7459da96e64691881d1f3dd7198e35af39cc634
Hyphona <alperrot@student.42angouleme.fr>
2024-04-25 14:05:44 +0200

    Add ft_exit() function

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 09:57:15 by alperrot          #+#    #+#             */
-/*   Updated: 2024/04/25 13:05:05 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/04/25 14:05:09 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,27 @@ static void	ft_error(void)
 {
 	ft_printf("%s", mlx_strerror(mlx_errno));
 	exit(EXIT_FAILURE);
+}
+
+void	ft_exit(t_game *game)
+{
+	t_level	*level;
+	
+	level = game->level;
+	if (level)
+	{
+		while (level->next)
+		{
+			mlx_delete_image(game->mlx, level->img);
+			level = level->next;
+		}
+		free_stack(game->level);
+	}
+	if (game->player)
+		mlx_delete_image(game->mlx, game->player);
+	mlx_terminate(game->mlx);
+	free(game);
+	exit(EXIT_SUCCESS);
 }
 
 int32_t	main(void)
@@ -37,8 +58,6 @@ int32_t	main(void)
 	mlx_loop_hook(mlx, ft_hook_close, mlx);
 	mlx_key_hook(mlx, ft_hook_move, game);
 	mlx_loop(mlx);
-	mlx_delete_image(mlx, game->player);
-	free(game);
-	mlx_terminate(mlx);
+	ft_exit(game);
 	return (EXIT_SUCCESS);
 }
