ae6e65fd03d71a36c936bbb86da1f017ea5ca901
Hyphona <alperrot@student.42angouleme.fr>
2024-05-13 09:51:52 +0200

    Fixed backtracking loop (but still doesn't work)

--- so_long/parsing/path_checker.c
+++ so_long/parsing/path_checker.c
@@ -6,7 +6,7 @@
 /*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 14:04:38 by alperrot          #+#    #+#             */
-/*   Updated: 2024/05/13 09:42:50 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/05/13 09:50:57 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,6 @@ static t_map	*create_map(t_map *map_node, char *map)
 
 static t_map	*get_node_at(t_game *g, t_map *map, int x, int y)
 {
-	//ft_printf("gna\n");
 	if (!map)
 		return (NULL);
 	if (map->x == x && map->y == y)
@@ -50,17 +49,25 @@ static t_map	*get_node_at(t_game *g, t_map *map, int x, int y)
 	while (map->y != y)
 		if (map->next)
 			map = map->next;
+		else
+			return (NULL);
 	while (map->x != x)
 		if (map->next)
 			map = map->next;
+		else
+			return (NULL);
 	if (map->x == x && map->y == y)
 		return (map);
 	while (map->y != y)
 		if (map->prev)
 			map = map->prev;
+		else
+			return (NULL);
 	while (map->x != x)
 		if (map->prev)
 			map = map->prev;
+		else
+			return (NULL);
 	if ((map->x != x) || (map->y != y))
 		return (NULL);
 	return (map);
@@ -70,11 +77,10 @@ void	backtrack_path(t_game *g, t_map *map_node, int x, int y)
 {
 	if (!map_node)
 		return ;
-	ft_printf("x: %d y: %d\n", x, y);
+	if (map_node->block_type == '1')
+		return ;
 	if (x <= (int)g->map_w * 32 && y <= (int)g->map_h * 32 && x >= 0 && y >= 0)
 	{
-		if (map_node->block_type == '1')
-			return ;
 		map_node->block_type = '1';
 		if (get_node_at(g, map_node, x + 32, y))
 			backtrack_path(g, get_node_at(g, map_node, x + 32, y), x + 32, y);
