21cd2f48c5eb6764f11bf072a6ad5b8ddf38676a
Hyphona <alperrot@student.42angouleme.fr>
2024-05-21 09:58:48 +0200

    Mini fix for args parsing

--- push_swap/main.c
+++ push_swap/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alperrot <alperrot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alperrot <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:27:56 by alperrot          #+#    #+#             */
-/*   Updated: 2024/05/19 15:26:16 by alperrot         ###   ########.fr       */
+/*   Updated: 2024/05/21 09:56:32 by alperrot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,30 @@ static t_stack	*init_stack_a(char **av)
 	return (s_a);
 }
 
+static int	check(int ac, char **av)
+{
+	int	i;
+
+	if (ac < 2)
+		return (0);
+	i = 0;
+	while (av[i])
+	{
+		if (av[i][0] == '\0')
+			return (0);
+		i++;
+	}
+	if (!check_args(av))
+		return (0);
+	return (1);
+}
+
 int	main(int ac, char **av)
 {
 	t_stack	*s_a;
 	t_stack	*s_b;
 
-	if (ac < 2)
-		return (0);
-	if (!check_args(av))
+	if (!check(ac, av))
 	{
 		write(2, "Error\n", 6);
 		return (0);
